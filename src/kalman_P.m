4ac27a5e34af86e46e3c1bc5e27578f7e15671ff
hadi <tabatabaee@ucdavis.edu>
2016-05-16 22:08:31 -0700

    Place holder for Kalman Filter

---
+++ src/kalman_P.m
@@ -0,0 +1,25 @@
+function [X, P] = kalman_P(X_p, P_p, Z, Q)
+% SYNTAX: [X, P] = kalman_P(X_p, P_p)
+%
+% INPUT:
+%     X_p = apriori state estimate
+%     P_p = apriori covariance matrix
+%     Z   = measurement input
+%     Q   = measurement covariance matrix
+%
+% OUTPUT:
+%     X   = state estimate
+%     P   = state covariance matrix
+%
+% DESCRIPTION:
+%     Performs Kalman Filter calculations using a position only state
+%     transition model for state predictions and the distance geometry
+%     method for integrating measurements.
+%
+% -------------------------------------------------------------------------
+%
+%   Copyright 2016, Hadi Tabatabaee, All rights reserved.
+%
+% -------------------------------------------------------------------------
+
+% PLACEHOLDER
